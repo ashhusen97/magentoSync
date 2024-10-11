e0034d314275f6263fc710e2b203aa39a7726903
Aashir Hussain <aashirhussain@Aashirs-MacBook-Pro.local>
2024-10-11 19:35:19 +0500

    Instance change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const Typesense = require('typesense');
+const express = require("express");
+const bodyParser = require("body-parser");
+const Typesense = require("typesense");
 
 // Create Express app
 const app = express();
@@ -10,43 +10,45 @@ app.use(bodyParser.json());
 
 // Initialize Typesense client
 let typesenseClient = new Typesense.Client({
-    nodes: [
-          {
-              host: "integration-typesense.foodservicedirect.ca",
-              port: 443,
-              path: "",
-              protocol: "https",
-          },
-      ],
-      apiKey: "vFWSoLHDWdbfnxVMu0D8Cf3d8LEqGetjFLr9fMT8Od2066NY",
-      connectionTimeoutSeconds: 100,
-  });
+  nodes: [
+    {
+      host: "integration-typesense.foodservicedirect.ca",
+      port: 443,
+      path: "",
+      protocol: "https",
+    },
+  ],
+  apiKey: "vFWSoLHDWdbfnxVMu0D8Cf3d8LEqGetjFLr9fMT8Od2066NY",
+  connectionTimeoutSeconds: 100,
+});
 
 // Webhook endpoint to receive product updates from Magento
-app.post('/webhook/magento-product-update', async (req, res) => {
+app.post("/webhook/magento-product-update", async (req, res) => {
   try {
-    console.log(req.body)
-    const productData = req.body;  // Assuming Magento sends product data in the webhook payload
+    console.log(req.body);
+    const productData = req.body; // Assuming Magento sends product data in the webhook payload
 
     // Update or upsert product in Typesense
     // const typesenseResponse = await typesenseClient.collections('staging_CA_v1').documents().upsert();
 
-    const typesenseResponse =   await typesenseClient
-    .collections("staging_CA_v1")
-    .documents()
-    .import({
-        id: productData.id,  // Assuming Magento sends the product ID
-        price: productData.price,
-     
-      }, { action: "update" });
+    const typesenseResponse = await typesenseClient
+      .collections("staging_CA_v2")
+      .documents()
+      .import(
+        {
+          id: "" + productData.id, // Assuming Magento sends the product ID
+          price: productData.price,
+        },
+        { action: "update" }
+      );
 
-    console.log('Product synced with Typesense:', typesenseResponse);
+    console.log("Product synced with Typesense:", typesenseResponse);
 
     // Respond to Magento that the webhook was received and processed
-    res.status(200).send('Webhook received and processed');
+    res.status(200).send("Webhook received and processed");
   } catch (error) {
-    console.error('Error processing webhook:', error);
-    res.status(500).send('Error processing webhook',error);
+    console.error("Error processing webhook:", error);
+    res.status(500).send("Error processing webhook", error);
   }
 });
 
